b8675a8bcfbabdf99b106a8fffa23c11bb7fa164
A-fauzi <akhmadfauzi0101@gmail.com>
2023-02-08 23:51:22 +0700

    update tz

--- app/utils/currentDate.js
+++ app/utils/currentDate.js
@@ -1,7 +1,7 @@
 const date = require("date-and-time");
 
 exports.currentDate = () => {
-  const now = new Date(new Date().toUTCString());
+  const now = new Date;
 
   const currentDate = date.format(now, "ddd, MMM DD YYYY | HH:mm:ss");
 
