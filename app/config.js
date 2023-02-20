4b7d0ebcd6d52b78df7f24acbb00ca4e9bd54576
A-fauzi <akhmadfauzi0101@gmail.com>
2023-02-20 21:26:56 +0700

    auto generate admin

--- app/config.js
+++ app/config.js
@@ -2,16 +2,20 @@ require('dotenv').config()
 
 const db = require("./models/index");
 const Role = db.role;
+const User = db.user;
+
+var bcrypt = require("bcrypt");
 
 // Local
 exports.dbConnect = (mongoose) => {
   mongoose.Promise = global.Promise;
   mongoose
-    // .connect(process.env.DB_URI_LOCAL, { useNewUrlParser: true })
-    .connect(process.env.DB_URI_ATLAS, { useNewUrlParser: true })
+    .connect(process.env.DB_URI_LOCAL, { useNewUrlParser: true })
+    // .connect(process.env.DB_URI_ATLAS, { useNewUrlParser: true })
     .then(() => {
       console.log("Successfully connect to the database");
       initial()
+      initUser()
     })
     .catch((err) => {
       console.log("Could not connect to the database. Error...", err);
@@ -50,6 +54,66 @@ function initial() {
         }
 
         console.log("added 'admin' to roles collection");
+      });
+    }
+  })
+}
+
+function initUser() {
+  User.estimatedDocumentCount((err, count) => {
+    if (!err && count === 0) {
+      const roles = ["admin", "user", "moderator"]
+
+      const user = new User({
+        username: "admin",
+        email: "admin@gmail.com",
+        password: bcrypt.hashSync("@AbkjkHKjh101010", 8),
+        fullName: "Admin",
+        division: "admin"
+      });
+
+      user.save((err, user) => {
+        if (err) {
+          console.log(err)
+        }
+
+        if (roles) {
+          Role.find(
+            {
+              name: { $in: roles }
+            },
+            (err, roles) => {
+              if (err) {
+                console.log(err)
+              }
+
+              user.roles = roles.map(role => role._id);
+              user.save(err => {
+                if (err) {
+                  console.log(err)
+                }
+
+                console.log
+                console.log("Admin was registered automatically successfully!");
+              });
+            }
+          );
+        } else {
+          Role.findOne({ name: "user" }, (err, role) => {
+            if (err) {
+              console.log(err);
+            }
+
+            user.roles = [role._id];
+            user.save(err => {
+              if (err) {
+                console.log(err);
+              }
+
+              console.log("Admin was registered automatically successfully!");
+            });
+          });
+        }
       });
     }
   })
