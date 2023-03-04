21ef45a243e61278ffc43fbcf083b3bab7ed8dd1
A-fauzi <akhmadfauzi0101@gmail.com>
2023-03-04 15:46:29 +0700

    add pagination

--- app/controllers/product/find.all.js
+++ app/controllers/product/find.all.js
@@ -3,18 +3,33 @@ const { currentDate } = require("../../utils/currentDate.js");
 const Item = require("../../models/product/model.product.js");
 const { successMessage } = require("../../custom-response/response.success.js");
 
-exports.findAll = (req, res) => {
-  Item.find()
-    .then((data) => {
-      res.send({
-        message: "data items",
-        count: `${data.length}`,
-        data: data,
-      });
-    })
-    .catch((err) => {
-      res.status(500).send({
-        message: err.message || "Some error occurred while retrieving item.",
-      });
+exports.findAll = async (req, res, next) => {
+  try {
+    // We destructure the req.query object to get the page and limit variables from url 
+    const { page = 1, limit = 10 } = req.query;
+
+    const item = await Item.find({ ...req.query })
+      // We multiply the "limit" variables by one just to make sure we pass a number and not a string
+      .limit(limit * 1)
+      // I don't think i need to explain the math here
+      .skip((page - 1) * limit)
+      // We sort the data by the date of their creation in descending order (user 1 instead of -1 to get ascending order)
+      .sort({ createdAt: -1 })
+
+    // Getting the numbers of products stored in database
+    const count = await Item.countDocuments();
+
+    return res.status(200).json({
+      message: "data items",
+      count: `${item.length}`,
+      totalPages: Math.ceil(count / limit),
+      currentPage: page,
+      item,
     });
+  } catch (err) {
+    res.status(500).send({
+      message: err.message || "Some error occurred while retrieving item.",
+    });
+    // next(err);
+  }
 };
