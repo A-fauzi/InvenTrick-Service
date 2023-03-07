0aa50ea440b6b5b132a2b237bf70d877051998d8
A-fauzi <akhmadfauzi0101@gmail.com>
2023-03-07 23:23:06 +0700

    admin request update

--- app/config.js
+++ app/config.js
@@ -70,7 +70,9 @@ function initUser() {
         email: "admin@gmail.com",
         password: bcrypt.hashSync(process.env.ADMIN_PASS, 8),
         fullName: "Admin",
-        division: "admin"
+        division: "admin",
+        path_storage: "null",
+        profile_image: "https://i.pinimg.com/564x/c2/e0/b1/c2e0b1aad596f510cca5c232bc15364f.jpg"
       });
 
       user.save((err, user) => {
