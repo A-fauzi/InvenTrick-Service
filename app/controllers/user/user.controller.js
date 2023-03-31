9161cb75e50f5aa631391aee2e158af673612faf
Akhmad Fauzi <akhmadfauzi0101@gmail.com>
2023-04-01 03:40:57 +0700

    add paging data users

--- app/controllers/user/user.controller.js
+++ app/controllers/user/user.controller.js
@@ -20,22 +20,57 @@ exports.moderatorBoard = (req, res) => {
     res.status(200).send("Moderator Content.");
 };
 
-exports.findAllUser = (req, res) => {
-    Item.find()
-        .sort({ _id: -1 })
-        .populate("roles", "-__v")
-        .then((data) => {
-            res.send({
-                message: "data items",
-                count: `${data.length}`,
-                data: data
-            });
-        })
-        .catch((err) => {
-            res.status(500).send({
-                message: err.message || "Some error occurred while retrieving item.",
-            });
+exports.findAllUser = async (req, res) => {
+
+    try {
+        // We destructure the req.query object to get the page and limit variables from url 
+        const { page = 1, limit = 10 } = req.query;
+
+        const item = await Item.find({ ...req.query })
+            // We multiply the "limit" variables by one just to make sure we pass a number and not a string
+            .limit(limit * 1)
+            // I don't think i need to explain the math here
+            .skip((page - 1) * limit)
+            // We sort the data by the date of their creation in descending order (user 1 instead of -1 to get ascending order)
+            .sort({ _id: -1 })
+            .populate("roles", "-__v")
+
+        // Getting the numbers of products stored in database
+        const count = await Item.countDocuments();
+
+        return res.status(200).json({
+            message: "data items",
+            totalCount: count,
+            countPerPage: `${item.length}`,
+            totalPages: Math.ceil(count / limit),
+            currentPage: page,
+            data: item,
         });
+
+    } catch (error) {
+        res.status(500).send({
+            message: err.message || "Some error occurred while retrieving item.",
+        });
+        // next(err);
+    }
+
+
+
+    // Item.find()
+    //     .sort({ _id: -1 })
+    //     .populate("roles", "-__v")
+    //     .then((data) => {
+    //         res.send({
+    //             message: "data items",
+    //             count: `${data.length}`,
+    //             data: data
+    //         });
+    //     })
+    //     .catch((err) => {
+    //         res.status(500).send({
+    //             message: err.message || "Some error occurred while retrieving item.",
+    //         });
+    //     });
 };
 
 exports.findOneUserById = (req, res) => {
